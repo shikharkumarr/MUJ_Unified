b60d9964754c7e2aa45ecbdf22f0a1eb08bda0e0
sehaj-79 <bhasin.seh79@gmail.com>
2021-12-23 01:33:19 +0530

    Login Java - 1st Commit. LoginActivity.java-JAVA.

--- app/src/main/java/com/aliferous/mujunified/LoginActivity.java
+++ app/src/main/java/com/aliferous/mujunified/LoginActivity.java
@@ -1,14 +1,96 @@
 package com.aliferous.mujunified;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button b1;
+    EditText et_id,et_pass;
+    FirebaseAuth auth;
+
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null){
+            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+            intent.putExtra("splash","1");
+            startActivity(intent);
+            finish();
+        }
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
+        b1= findViewById(R.id.b_submit);
+        et_id=findViewById(R.id.edit_text_email);
+        et_pass=findViewById(R.id.edit_text_password);
+
+        b1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                return false;
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_email = Objects.requireNonNull(et_id.getText()).toString();
+                String txt_password = Objects.requireNonNull(et_pass.getText()).toString();
+
+                if(TextUtils.isEmpty(txt_password)  ||  TextUtils.isEmpty(txt_email)){
+                    Toast.makeText(LoginActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
+                }
+
+
+                else{
+                    auth.signInWithEmailAndPassword(txt_email,txt_password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else {
+                                        Toast.makeText(LoginActivity.this, "Authentication Failed!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
     }
 }
