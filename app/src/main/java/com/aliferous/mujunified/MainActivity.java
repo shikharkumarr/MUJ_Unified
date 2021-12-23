0fd55e12c4001fc6c155d291454019eeddc0e15a
sehaj-79 <bhasin.seh79@gmail.com>
2021-12-23 18:18:42 +0530

    Minor Bug fixes Ux Fixes - 1st Commit

--- app/src/main/java/com/aliferous/mujunified/MainActivity.java
+++ app/src/main/java/com/aliferous/mujunified/MainActivity.java
@@ -2,13 +2,30 @@ package com.aliferous.mujunified;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+    FrameLayout ProfileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ProfileButton = findViewById(R.id.Map);
+
+        ProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/aliferous/mujunified/ProfileActivity.java
+++ app/src/main/java/com/aliferous/mujunified/ProfileActivity.java
@@ -1,14 +1,51 @@
 package com.aliferous.mujunified;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView Name,Course,YearSem;
+
+    FirebaseUser firebaseUser;
+    FirebaseFirestore reference = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Name = findViewById(R.id.Profile_tv_name);
+        Course = findViewById(R.id.Profile_tv_Course);
+        YearSem = findViewById(R.id.Profile_tv_YearSem);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference.collection("Users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d("TAG", document.getId() + " => " + document.getData());
+                        //reference.collection("Users").document(document.getId()).addSnapshotListener(new );
+                    }
+                } else {
+                    Log.w("TAG", "Error getting documents.", task.getException());
+                }
+            }
+        });
+
     }
 }
