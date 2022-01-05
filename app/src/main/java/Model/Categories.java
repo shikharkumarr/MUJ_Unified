e72ce517649724d0756be7c92f263b95a9160d18
sehaj-79 <bhasin.seh79@gmail.com>
2022-01-05 18:18:18 +0530

    Categories Model Java - Update Commit Categories.java - Java

---
+++ app/src/main/java/Model/Categories.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class Categories {
+
+    private String Name;
+
+    public Categories(String Name) {
+        this.Name = Name;
+
+    }
+
+    public Categories() {
+
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
